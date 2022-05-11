2308fa051c7c1e16e6c45aac44540db47ff97a4c
Jeong-Hyeon-Ji <wjdguswl5500@gmail.com>
2022-05-11 10:33:43 +0900

    add code

--- API/api.py
+++ API/api.py
@@ -1,51 +1,65 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
-# from flask_restx import Resource, Api
-from datetime import datetime
+from fileinput import filename
+from flask import Flask, request, jsonify, render_template
 import os
 import numpy as np
-import cv2
+from collections import OrderedDict
+from datetime import datetime
+from werkzeug.utils import secure_filename
+from easyocr import Reader
+# from PIL import Image
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import torch.nn.functional as F
 
 app = Flask(__name__)
-# api = Api(app)
 
-# 전처리
-def pretreatment(file):
-    pass
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 예측
-def predict():
-    pass
+def get_files(path):
+    file_list = []
+
+    files = [f for f in os.listdir(path) if not f.startswith('.')]  # skip hidden file
+    files.sort()
+    abspath = os.path.abspath(path)
+    for file in files:
+        file_path = os.path.join(abspath, file)
+        file_list.append(file_path)
+
+    return file_list, len(file_list)
 
 @app.route('/')
-def hello(): # test
+def get():
     return render_template("test.html")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    save_path = "./data/images/"
-    
+@app.route('/image', methods=['POST'])
+def image(): 
+    save_path = "./workspace/demo_images/"
+    model_path = "./workspace/user_network_dir"
+
     # 접근거부로 우선 주석처리
     # if os.listdir(save_path):
     #     os.remove(save_path)
 
-    if os.path.exists(save_path):
-        return jsonify(result=0, message='exists')
+    date = request.form['date']
+    category = request.form['category']
+    files = request.files.getlist("images[]")
+    for f in files:
+        file_name = datetime.now().strftime('%Y%H%M%S')+".jpeg"
+        f.save(save_path+secure_filename(file_name))
+    
+    reader = Reader(['ko'], gpu=True,
+                    model_storage_directory=model_path,
+                    user_network_directory=model_path,
+                    recog_network='custom')
 
-    files = request.files['img']
-    if files:
-        file_name = datetime.now().strftime('%Y%H%M%S')
-        files.save('{0}{1}.png'.format(save_path, file_name))
-        return jsonify(result=1, message='saved')
-    else:
-        return jsonify(result=0, message='empty file')
+    # files, count = get_files(save_path)
+    return jsonify("ss")
 
 
 if __name__ == "__main__":
     app.run(debug=True) 
 
-# REST API https://problem-solving.tistory.com/9
-#  Flask를 사용하여 Python에서 PyTorch를 REST API로 배포 https://tutorials.pytorch.kr/intermediate/flask_rest_api_tutorial.html
-#  Flask - 웹서비스 RESTFUL API https://niceman.tistory.com/192
-# api 호출: https://toramko.tistory.com/6
 # set FLASK_APP=api.py 
 # flask run --host=0.0.0.0
